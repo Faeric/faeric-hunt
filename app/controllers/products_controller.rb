ab9a84e9552bcbeb6d89f908d2f0246b854f1750
Eric <fagot.eric@gmail.com>
2016-04-22 22:39:47 +0200

    Add new and create controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,31 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    #binding.pry
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :url)
+  end
+
 end
